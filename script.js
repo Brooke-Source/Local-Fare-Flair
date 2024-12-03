741182308af668d0f434cd3a07fc2fed91130a90
Brooke Brackett <brackettb105@my.clevelandcc.edu>
2024-12-03 18:16:58 -0500

    Wasn't the JScript

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 
- 
+    // JavaScript function to show an alert when a button is clicked
+    function showAlert() {
+        alert("Button clicked!");
+      }
+   
+    
+      function openMap(address) {
+        var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+        var encodedAddress = encodeURIComponent(address);
+        
+        // If on a mobile device (iOS or Android), try opening in respective map apps
+        if (isMobile) {
+            // For iOS devices (Apple Maps)
+            if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
+                window.location.href = "https://maps.apple.com/?q=" + encodedAddress;
+            }
+            // For Android devices (Google Maps)
+            else {
+                window.location.href = "https://www.google.com/maps?q=" + encodedAddress;
+            }
+        }
+        // If on desktop, default to Google Maps
+        else {
+            window.open("https://www.google.com/maps?q=" + encodedAddress, '_blank');
+        }
+    }
+  
     
     
